02cc508df3eba4300970582b203c2da3fe216174
monirozzamanroni <monirozzaman35-223@diu.edu.bd>
2019-12-25 13:41:07 +0600

    first commit

---
+++ Roket-App-Offline/app/src/main/java/com/itvillage/dev/offlinerocketapp/model/ContactEmail.java
@@ -0,0 +1,11 @@
+package com.itvillage.dev.offlinerocketapp.model;
+
+public class ContactEmail {
+    public String address;
+    public String type;
+
+    public ContactEmail(String address, String type) {
+        this.address = address;
+        this.type = type;
+    }
+}
